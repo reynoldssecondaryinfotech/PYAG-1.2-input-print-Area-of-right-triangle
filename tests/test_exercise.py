4de4c0d6c80df6af3f7508cae89d8c4a96536973
kkarim <kkarim@sd61learn.ca>
2021-05-04 13:39:59 -0700

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -2,8 +2,8 @@ import pytest
 import src.exercise
 
 def test_exercise():
-    input_values = ["1","2","3"]
-    input_values_stored = ["1","2","3"]
+    input_values = ["3","5"]
+    input_values_stored = ["3","5"]
     output = []
 
     def mock_input(s=None):
@@ -19,7 +19,4 @@ def test_exercise():
 
     src.exercise.main()
 
-    assert [output[0],output[1],output[2],output[3]] == ["Give the first number:",\
-                                               "Give the second number:",\
-                                               "Give the third number:",\
-                                               "The sum of the numbers is " + str(int(input_values_stored[0])+int(input_values_stored[1])+int(input_values_stored[2]))]
+    assert [output[0]] == ["7.5"]
