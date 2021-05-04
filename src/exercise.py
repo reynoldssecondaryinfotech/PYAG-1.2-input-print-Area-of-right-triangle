64eb7bf66bf71a52ca6b76596e3b875895561634
kkarim <kkarim@sd61learn.ca>
2021-05-04 13:36:09 -0700

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,10 +1,7 @@
 def main():
     #write your code below this line
-    #This program reads two numbers and prints their sum:
-    a = int(input("Give the first number:"))
-    b = int(input("Give the second number:"))
-
-    print("The sum of the numbers is " + str(a + b))
+    # Read the numbers b and h like this:
+    b = int(input())
 
 if __name__ == '__main__':
     main()
