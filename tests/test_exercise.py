65a4ab5eca4563b7737aec9681f8218780e6ae6d
kkarim <kkarim@sd61learn.ca>
2021-05-04 13:50:44 -0700

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -2,8 +2,8 @@ import pytest
 import src.exercise
 
 def test_exercise():
-    input_values = ["3","5"]
-    input_values_stored = ["3","5"]
+    input_values = ["3","5","10","2","179","1534","1543","57"]
+    input_values_stored = ["3","5","10","2","179","1534","1543","57"]
     output = []
 
     def mock_input(s=None):
@@ -16,7 +16,18 @@ def test_exercise():
 
     src.exercise.input = mock_input
     src.exercise.print = lambda s : output.append(s)
-
     src.exercise.main()
 
-    assert [output[0]] == ["7.5"]
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+    src.exercise.main()
+    
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+    src.exercise.main()
+    
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+    src.exercise.main()
+    
+    assert [output[0],output[1],output[2],output[3]] == ["7.5","10.0","137293.0","43975.5"]
